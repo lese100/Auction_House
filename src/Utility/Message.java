b1e114813390a671b82cc80f531da2dd638caf42
Warren Craft <wdcraft@unm.ed>
2018-11-27 18:25:01 -0700

    Update the MessageIdentifier enum items in the Message class in the Utility package.

--- src/Utility/Message.java
+++ src/Utility/Message.java
@@ -15,9 +15,80 @@ import java.io.Serializable;
  */
 public class Message <T> implements Serializable {
 
-    // enum to be further elaborated with other identifiers
-    public enum MessageIdentifier {AGENT_OPENING_ACCOUNT,
-                                   AUCTION_HOUSE_OPENING_ACCOUNT};
+    /*
+    enum comments use A = Agent, B = BANK, AH = Auction House
+    most recent count is 29 items, defined in the code in alphab order
+    organized in comments below by category
+    IDs for Agent-Sent Messages:
+        ACKNOWLEDGED               (sent to AH)
+        CLOSE_REQUEST              (sent to B or AH)
+        GET_LIST_OF_AUCTION_HOUSES (sent to B)
+        GET_SECRET_KEY             (sent to B)
+        JOIN_AUCTION_HOUSE         (sent to AH)
+        MAKE_BID                   (sent to AH)
+        OPEN_AGENT_ACCT            (sent to B)
+        REQUEST_BALANCE            (sent to B)
+        TRANSFER_FUNDS             (sent to B)
+
+    IDS for AH-Sent Messages:
+        BID_ACCEPTED                (sent to A, bid from A was accepted)
+        BID_OUTBIDDED               (sent to A, A was outbidded)
+        BID_REJECTED_INADEQUATE     (sent to A, less than min bid)
+        BID_REJECTED_NSF            (sent to A, insufficient funds)
+        BID_WON                     (sent to A, bid from A won auction)
+        MAKE_BID                    (sent to AH)
+        CHECK_FUNDS                 (sent to B, check A's funds for bid)
+        CLOSE_ACCEPTED              (sent to A if OK to close out)
+        CLOSE_REJECTED              (sent to A if not OK to close out)
+        LIST_OF_AUCTION_HOUSE_ITEMS (sent to A)
+        OPEN_AUCTIONHOUSE_ACCT      (sent to B)
+        UNFREEZE_FUNDS              (sent to B)
+        UPDATE_AUCTION_ITEMS        (sent to A)
+
+    IDS for Bank-Sent Messages:
+        AGENT_ACCT_CONFIRMED        (sent to A)
+        AUCTIONHOUSE_ACCT_CONFIRMED (sent to AH)
+        BALANCE                     (sent to A)
+        CHECK_FAILURE               (sent to AH)
+        CHECK_SUCCESS               (sent to AH)
+        CLOSE_ACCEPTED              (sent to A or AH)
+        CLOSE_REJECTED              (sent to A or AH)
+        LIST_OF_AUCTION_HOUSES      (sent to A)
+        SECRET_KEY                  (sent to A)
+        TRANSFER_SUCCESS            (sent to A)
+    */
+
+    public enum MessageIdentifier {
+        ACKNOWLEDGED,                // A reply to BID_OUTBIDDED or BID_WON
+        AGENT_ACCT_CONFIRMED,        // B reply to OPEN_AGENT_ACCT
+        AUCTIONHOUSE_ACCT_CONFIRMED, // B reply to OPEN_AUCTIONHOUSE_ACCT
+        BALANCE,                     // B reply to REQUEST_BALANCE
+        BID_ACCEPTED,                // AH reply to MAKE_BID
+        BID_OUTBIDDED,               // AH send to A
+        BID_REJECTED_INADEQUATE,     // AH reply to MAKE_BID
+        BID_REJECTED_NSF,            // AH reply to MAKE_BID
+        BID_WON,                     // AH send to A
+        CHECK_FUNDS,                 // AH send to B
+        CHECK_FAILURE,               // B reply to CHECK_FUNDS
+        CHECK_SUCCESS,               // B reply to CHECK_FUNDS
+        CLOSE_ACCEPTED,              // B or AH reply to CLOSE_REQUEST
+        CLOSE_REJECTED,              // B or AH reply to CLOSE_REQUEST
+        CLOSE_REQUEST,               // A request to B or AH; AH request to B
+        GET_LIST_OF_AUCTION_HOUSES,  // A request list of AHs from B
+        GET_SECRET_KEY,              // A request secret key from B for AH
+        JOIN_AUCTION_HOUSE,          // A request to AH
+        LIST_OF_AUCTION_HOUSE_ITEMS, // AH reply to JOIN_AUCTION_HOUSE
+        LIST_OF_AUCTION_HOUSES,      // B reply to GET_LIST_OF_AUCTION_HOUSES
+        MAKE_BID,                    // A send to AH
+        OPEN_AGENT_ACCT,             // A request to B
+        OPEN_AUCTIONHOUSE_ACCT,      // AH request to B
+        REQUEST_BALANCE,             // A request to B
+        SECRET_KEY,                  // B reply to GET_SECRET_KEY
+        TRANSFER_FUNDS,              // A request to B
+        TRANSFER_SUCCESS,            // B reply to TRANSFER_FUNDS
+        UNFREEZE_FUNDS,              // AH request to B
+        UPDATE_AUCTION_ITEMS         // AH send to A
+    };
 
     private MessageIdentifier messageIdentifier;
     private T messageContent;
