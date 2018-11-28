96deaa5f91d97e82d51019e6952470bd606c9a17
Warren Craft <wdcraft@unm.ed>
2018-11-28 10:53:34 -0700

    Update the Message class to include CASE_NOT_FOUND identifier and update explanatory comments.

--- src/Utility/Message.java
+++ src/Utility/Message.java
@@ -17,45 +17,50 @@ public class Message <T> implements Serializable {
 
     /*
     enum comments use A = Agent, B = BANK, AH = Auction House
-    most recent count is 29 items, defined in the code in alphab order
-    organized in comments below by category
+    Most recent count is 30 items, defined in the code in alphab order.
+    Organized in comments below by category. "reply" indicates a reply to
+    a message initiated by another entity; "send" indicates an message
+    initiated by the entity.
+
     IDs for Agent-Sent Messages:
-        ACKNOWLEDGED               (sent to AH)
-        CLOSE_REQUEST              (sent to B or AH)
-        GET_LIST_OF_AUCTION_HOUSES (sent to B)
-        GET_SECRET_KEY             (sent to B)
-        JOIN_AUCTION_HOUSE         (sent to AH)
-        MAKE_BID                   (sent to AH)
-        OPEN_AGENT_ACCT            (sent to B)
-        REQUEST_BALANCE            (sent to B)
-        TRANSFER_FUNDS             (sent to B)
+        ACKNOWLEDGED               (reply to AH)
+        CASE_NOT_FOUND             (reply to B or AH)
+        CLOSE_REQUEST              (send to B or AH)
+        GET_LIST_OF_AUCTION_HOUSES (send to B)
+        GET_SECRET_KEY             (send to B)
+        JOIN_AUCTION_HOUSE         (send to AH)
+        MAKE_BID                   (send to AH)
+        OPEN_AGENT_ACCT            (send to B)
+        REQUEST_BALANCE            (send to B)
+        TRANSFER_FUNDS             (send to B)
 
     IDS for AH-Sent Messages:
-        BID_ACCEPTED                (sent to A, bid from A was accepted)
-        BID_OUTBIDDED               (sent to A, A was outbidded)
-        BID_REJECTED_INADEQUATE     (sent to A, less than min bid)
-        BID_REJECTED_NSF            (sent to A, insufficient funds)
-        BID_WON                     (sent to A, bid from A won auction)
-        MAKE_BID                    (sent to AH)
-        CHECK_FUNDS                 (sent to B, check A's funds for bid)
-        CLOSE_ACCEPTED              (sent to A if OK to close out)
-        CLOSE_REJECTED              (sent to A if not OK to close out)
-        LIST_OF_AUCTION_HOUSE_ITEMS (sent to A)
-        OPEN_AUCTIONHOUSE_ACCT      (sent to B)
-        UNFREEZE_FUNDS              (sent to B)
-        UPDATE_AUCTION_ITEMS        (sent to A)
+        BID_ACCEPTED                (reply to A, bid from A was accepted)
+        BID_OUTBIDDED               (send to A, A was outbidded)
+        BID_REJECTED_INADEQUATE     (reply to A, less than min bid)
+        BID_REJECTED_NSF            (reply to A, insufficient funds)
+        BID_WON                     (send to A, bid from A won auction)
+        CASE_NOT_FOUND              (reply to A or B)
+        CHECK_FUNDS                 (send to B, check A's funds for bid)
+        CLOSE_ACCEPTED              (reply to A if OK to close out)
+        CLOSE_REJECTED              (reply to A if not OK to close out)
+        LIST_OF_AUCTION_HOUSE_ITEMS (reply to A)
+        OPEN_AUCTIONHOUSE_ACCT      (send to B)
+        UNFREEZE_FUNDS              (send to B)
+        UPDATE_AUCTION_ITEMS        (send to A)
 
     IDS for Bank-Sent Messages:
-        AGENT_ACCT_CONFIRMED        (sent to A)
-        AUCTIONHOUSE_ACCT_CONFIRMED (sent to AH)
-        BALANCE                     (sent to A)
-        CHECK_FAILURE               (sent to AH)
-        CHECK_SUCCESS               (sent to AH)
-        CLOSE_ACCEPTED              (sent to A or AH)
-        CLOSE_REJECTED              (sent to A or AH)
-        LIST_OF_AUCTION_HOUSES      (sent to A)
-        SECRET_KEY                  (sent to A)
-        TRANSFER_SUCCESS            (sent to A)
+        AGENT_ACCT_CONFIRMED        (reply to A)
+        AUCTIONHOUSE_ACCT_CONFIRMED (reply to AH)
+        BALANCE                     (reply to A)
+        CASE_NOT_FOUND              (reply to A or AH)
+        CHECK_FAILURE               (reply to AH)
+        CHECK_SUCCESS               (reply to AH)
+        CLOSE_ACCEPTED              (reply to A or AH)
+        CLOSE_REJECTED              (reply to A or AH)
+        LIST_OF_AUCTION_HOUSES      (reply to A)
+        SECRET_KEY                  (reply to A)
+        TRANSFER_SUCCESS            (reply to A)
     */
 
     public enum MessageIdentifier {
@@ -68,25 +73,26 @@ public class Message <T> implements Serializable {
         BID_REJECTED_INADEQUATE,     // AH reply to MAKE_BID
         BID_REJECTED_NSF,            // AH reply to MAKE_BID
         BID_WON,                     // AH send to A
+        CASE_NOT_FOUND,              // All entities reply to all entities
         CHECK_FUNDS,                 // AH send to B
         CHECK_FAILURE,               // B reply to CHECK_FUNDS
         CHECK_SUCCESS,               // B reply to CHECK_FUNDS
         CLOSE_ACCEPTED,              // B or AH reply to CLOSE_REQUEST
         CLOSE_REJECTED,              // B or AH reply to CLOSE_REQUEST
-        CLOSE_REQUEST,               // A request to B or AH; AH request to B
-        GET_LIST_OF_AUCTION_HOUSES,  // A request list of AHs from B
-        GET_SECRET_KEY,              // A request secret key from B for AH
-        JOIN_AUCTION_HOUSE,          // A request to AH
+        CLOSE_REQUEST,               // A send to B or AH; AH send to B
+        GET_LIST_OF_AUCTION_HOUSES,  // A send to B
+        GET_SECRET_KEY,              // A send to B for AH
+        JOIN_AUCTION_HOUSE,          // A send to AH
         LIST_OF_AUCTION_HOUSE_ITEMS, // AH reply to JOIN_AUCTION_HOUSE
         LIST_OF_AUCTION_HOUSES,      // B reply to GET_LIST_OF_AUCTION_HOUSES
         MAKE_BID,                    // A send to AH
-        OPEN_AGENT_ACCT,             // A request to B
-        OPEN_AUCTIONHOUSE_ACCT,      // AH request to B
-        REQUEST_BALANCE,             // A request to B
+        OPEN_AGENT_ACCT,             // A send to B
+        OPEN_AUCTIONHOUSE_ACCT,      // AH send to B
+        REQUEST_BALANCE,             // A send to B
         SECRET_KEY,                  // B reply to GET_SECRET_KEY
-        TRANSFER_FUNDS,              // A request to B
+        TRANSFER_FUNDS,              // A send to B
         TRANSFER_SUCCESS,            // B reply to TRANSFER_FUNDS
-        UNFREEZE_FUNDS,              // AH request to B
+        UNFREEZE_FUNDS,              // AH send to B
         UPDATE_AUCTION_ITEMS         // AH send to A
     };
 
